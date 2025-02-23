32d09655e2b7c2fd4049920efe3e0f9b9cce8465
Aaron <piafyoyo06@gmail.com>
2025-02-23 20:38:31 +0800

    完成網站頁首與頁尾

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -3,27 +3,63 @@ import { Outlet, Link } from "react-router-dom";
 const FrontendLayout = () => {
   return (
     <div>
-      <header>
-        <nav className="mt-5">
-          <Link className="h4 mt-5 mx-2" to="/">
-            首頁
+      {/* 導覽列 */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container">
+          <Link className="navbar-brand fw-bold" to="/">
+            我的網站
           </Link>
-          <Link className="h4 mt-5 mx-2" to="/product">
-            產品頁面
-          </Link>
-          <Link className="h4 mt-5 mx-2" to="/cart">
-            購物車頁面
-          </Link>
-          <Link className="h4 mt-5 mx-2" to="/login">
-            登入頁面
-          </Link>
-        </nav>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer className="mt-5 text-center">
-        <p>© 2024 我的網站</p>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  首頁
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/product">
+                  產品頁面
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">
+                  購物車
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  登入
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* 主要內容區塊 */}
+      <Outlet />
+
+      {/* 頁尾 */}
+      <footer className="bg-dark text-white text-center py-3 mt-5">
+        <p className="mb-0">
+          © 六角 2024 React 作品實戰冬季班第六週作業 - Vite、React Router
+          <br />
+          by{" "}
+          <a href="https://www.worksbyaaron.com" target="_blank">
+            Aaron
+          </a>
+          . All rights reserved.
+        </p>
       </footer>
     </div>
   );
