32105704727a717c4beaf30c21766262df7ce1b8
Aaron <piafyoyo06@gmail.com>
2025-02-23 20:45:07 +0800

    引入通用樣式

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
+import "../src/assets/style.css";
+
 const App = () => {
   return <RouterProvider router={router} />;
 };
